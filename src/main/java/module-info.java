5593c35855bbde91ab58d2c9003e487e157cc06e
Boba? <bondanbanuaji@gmail.com>
2025-03-05 15:54:36 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,24 @@
+module com.jmc.mazebank {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.jmc.mazebank to javafx.fxml;
+    exports com.jmc.mazebank;
+    exports com.jmc.mazebank.Controllers;
+    exports com.jmc.mazebank.Controllers.Admin;
+    exports com.jmc.mazebank.Controllers.Client;
+    exports com.jmc.mazebank.Models;
+    exports com.jmc.mazebank.Views;
+}
