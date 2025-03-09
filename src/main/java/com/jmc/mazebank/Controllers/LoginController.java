951b697d8129bcf11fec932c82b8536ece75f8fd
Boba? <bondanbanuaji@gmail.com>
2025-03-10 06:48:27 +0700

    updated call function

--- src/main/java/com/jmc/mazebank/Controllers/LoginController.java
+++ src/main/java/com/jmc/mazebank/Controllers/LoginController.java
@@ -1,15 +1,24 @@
 package com.jmc.mazebank.Controllers;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public ChoiceBox acc_selector;
     public Label payee_address_lbl;
     public TextField payee_address_field;
     public TextField password_fld;
     public Button login_btn;
     public Label error_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
 }
