a798b2956f9df4a8298d0a9d9110e1adb27e468b
Boba? <bondanbanuaji@gmail.com>
2025-03-13 17:08:30 +0700

    updated API

--- src/main/java/com/jmc/mazebank/App.java
+++ src/main/java/com/jmc/mazebank/App.java
@@ -1,15 +1,12 @@
 package com.jmc.mazebank;
 
-import com.jmc.mazebank.Views.ViewFactory;
+import com.jmc.mazebank.Models.Model;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        ViewFactory viewFactory = new ViewFactory();
-        viewFactory.showLoginWindow();
+    public void start(Stage stage) {
+        Model.getInstance().getViewFactory().showLoginWindow();
     }
 }
