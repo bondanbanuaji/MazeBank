2589f82764633cdc12cd79966c0868017abffef0
Boba? <bondanbanuaji@gmail.com>
2025-03-15 11:53:50 +0700

    add transactions and update controllers

--- src/main/java/com/jmc/mazebank/Controllers/Client/ClientMenuController.java
+++ src/main/java/com/jmc/mazebank/Controllers/Client/ClientMenuController.java
@@ -1,5 +1,6 @@
 package com.jmc.mazebank.Controllers.Client;
 
+import com.jmc.mazebank.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
@@ -16,6 +17,19 @@ public class ClientMenuController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        addListeners();
+    }
 
+    private void addListeners() {
+        dashboard_btn.setOnAction(event -> onDashboard());
+        transaction_btn.setOnAction(event -> onTransactions());
+    }
+
+    private void onDashboard() {
+        Model.getInstance().getViewFactory().getClientSelectedMenuItem().set("Dashboard");
+    }
+
+    private void onTransactions() {
+        Model.getInstance().getViewFactory().getClientSelectedMenuItem().set("Transactions");
     }
 }
--- src/main/java/com/jmc/mazebank/Views/ViewFactory.java
+++ src/main/java/com/jmc/mazebank/Views/ViewFactory.java
@@ -1,6 +1,8 @@
 package com.jmc.mazebank.Views;
 
 import com.jmc.mazebank.Controllers.Client.ClientController;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -8,9 +10,17 @@ import javafx.stage.Stage;
 
 public class ViewFactory {
     // Client Views
+    private final StringProperty clientSelectedMenuItem;
     private AnchorPane dashboardView;
+    private AnchorPane transactionsView;
 
-    public ViewFactory(){}
+    public ViewFactory(){
+        this.clientSelectedMenuItem = new SimpleStringProperty("");
+    }
+
+    public StringProperty getClientSelectedMenuItem() {
+        return clientSelectedMenuItem;
+    }
 
     public AnchorPane getDashboardView(){
         if (dashboardView == null){
@@ -21,6 +31,17 @@ public class ViewFactory {
             }
         }
         return dashboardView;
+    }
+
+    public AnchorPane getTransactionsView() {
+        if (transactionsView == null){
+            try {
+                transactionsView = new FXMLLoader(getClass().getResource("/Fxml/Client/Transactions.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return transactionsView;
     }
 
     public void showLoginWindow() {
@@ -46,5 +67,9 @@ public class ViewFactory {
         stage.setScene(scene);
         stage.setTitle("Maze Bank");
         stage.show();
+    }
+
+    public void closeStage(Stage stage) {
+        stage.close();
     }
 }
